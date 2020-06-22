4bd48b26384d78baccf74db4266a0b85e9bee922
MuhammadHumair <research.scholar818@gmail.com>
2020-06-22 19:52:37 +0500

    Destination categories on index are fully dynamic.

--- travello/views.py
+++ travello/views.py
@@ -1,7 +1,23 @@
 from django.shortcuts import render
-from django.http import HttpResponse
+
+from .models import Destinations
 
 # Create your views here.
 
 def index(request):
-    return render(request, "index.html")
+    '''
+    This funcion is used to load model data and render the index page.
+    '''
+
+    des = Destinations.objects.all()
+    dic = {}
+    for d_values in des:
+        k = 'val' + str(d_values.id)
+        dic[k] = {
+            'name': d_values.name,
+            'desc': d_values.desc,
+            'price': d_values.price,
+            'image': str(d_values.img)
+        }
+    context = {'dest':dic}
+    return render(request, "index.html", context)
